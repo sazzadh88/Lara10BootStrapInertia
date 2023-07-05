4d2f26600eeba9d321367ba0a4777a6c1190864f
Sazzad Hussain <sazzadh88@gmail.com>
2023-07-05 09:58:29 +0530

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    @routes
+    @vite(["resources/js/app.js", "resources/js/Pages/{$page['component']}.vue", "resources/scss/app.scss"])
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
